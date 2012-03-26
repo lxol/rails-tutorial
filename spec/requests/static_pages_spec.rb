bbeed629ec0d4ff87f584da0751cb480cd624e29
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-03-26 20:26:12 +0100

    Add a Help static page and test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,13 +8,12 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
   end
+  describe "Help page" do
+
+    it "Should have a content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+
 end
-# describe "Static Pages" do
-#   describe "GET /static_pages" do
-#     it "works! (now write some real specs)" do
-#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#       get static_pages_index_path
-#       response.status.should be(200)
-#     end
-#   end
-# end
