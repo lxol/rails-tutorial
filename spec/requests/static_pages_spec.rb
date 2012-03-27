11ef9b10a6b8a6a408c1ce427dfc8e91eeff7d2e
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-03-27 07:13:08 +0100

    Remove application layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
   describe "Help page" do
 
@@ -14,6 +19,14 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+
+
   end
 
   describe "About page" do
@@ -22,6 +35,12 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_content('About Us')
     end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+				:text => "Ruby on Rails Tutorial Sample App | About")
+    end
+
   end
 
 end
