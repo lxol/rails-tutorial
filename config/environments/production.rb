0100ada6c7c57878d04cde89f3987c1c09c0591a
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-03 08:16:21 +0100

    Precompile Blueprint CSS in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ FirstApp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  # Precompile Blueprint CSS
+  config.assets.precompile += [ "blueprint/*.css" ]
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
