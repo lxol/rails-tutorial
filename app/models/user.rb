996708a78dfffe176981f4414aaf57c46c709609
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-11 17:55:12 +0100

    Add users presence validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,13 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
 
+  describe "When name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  describe "When email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
 end
