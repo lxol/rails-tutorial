239338e2e3036e833da5dd157f97e8097c28578e
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-13 18:40:44 +0100

    Show user. (cont.)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,3 +6,13 @@ describe "UserPages" do
   it {should have_selector('h1', text:  'Sign up')}
   it {should have_selector('title', text: full_title('Sign up'))}
 end
+
+describe "profile page" do
+  # Code to make a user variable
+  subject { page }
+  let(:user) {FactoryGirl.create(:user)}
+  before { visit user_path(user) }
+  it {should have_selector('h1', text: user.name) }
+  it {should have_selector('title', text: user.name) }
+
+end
