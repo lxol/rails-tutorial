6783a89ab752ff8ceb6b8e1841f8643600e1631c
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-14 11:36:56 +0100

    Update singup form.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,6 +5,28 @@ describe "UserPages" do
   before {visit signup_path}
   it {should have_selector('h1', text:  'Sign up')}
   it {should have_selector('title', text: full_title('Sign up'))}
+
+  describe "signup" do
+    before { visit signup_path }
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect {click_button "Create my account" }.not_to change(User, :count)
+      end
+    end
+    describe "with valid information" do
+      before do
+        fill_in "Name",  with: "Example User"
+        fill_in "Email",  with: "user@example.com"
+        fill_in "Password",  with: "foobar"
+        fill_in "Confirmation",  with: "foobar"
+      end
+      it "should create user" do
+        expect do
+          click_button "Create my account"
+        end.to_change(User, :count).by(1)
+      end
+    end
+  end
 end
 
 describe "profile page" do
