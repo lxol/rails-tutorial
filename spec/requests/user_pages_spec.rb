91002bc45d68e3c5ec48c1c94e5b4854a460d19d
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-16 20:58:04 +0100

    Signin right after signup.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,7 @@ describe "UserPages" do
         let(:user) {User.find_by_email('user@example.com') }
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
