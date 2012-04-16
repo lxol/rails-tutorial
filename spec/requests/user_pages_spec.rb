faeb81c10a7f0e24bcb80abf26fcb0bbd185c029
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-16 10:58:08 +0100

    Add test: after saving the user it should have success alert.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,6 +25,12 @@ describe "UserPages" do
           click_button "Create my account"
         end.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before {click_button "Create my account"}
+        let(:user) {User.find_by_email('user@example.com') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
 end
