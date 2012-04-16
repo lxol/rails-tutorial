6483e1d7791df77b13141a73aff6b810e6941d80
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-16 16:29:37 +0100

    Create initial sign in page with tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,11 +7,11 @@ describe "AuthenticationPages" do
     it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: 'Sign in') }
   end
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get authentication_pages_index_path
-      response.status.should be(200)
-    end
-  end
+  # describe "GET /authentication_pages" do
+  #   it "works! (now write some real specs)" do
+  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  #     get authentication_pages_index_path
+  #     response.status.should be(200)
+  #   end
+  # end
 end
