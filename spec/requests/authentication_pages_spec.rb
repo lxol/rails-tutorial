6b48941bd779a065e8fe9954342b00dae3a3851f
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-16 16:20:13 +0100

    Generate Sessions controller and integration test.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+  subject { page }
+  describe "signin page" do
+    before { visit signin_path }
+    it { should have_selector('h1', text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+  end
+  describe "GET /authentication_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get authentication_pages_index_path
+      response.status.should be(200)
+    end
+  end
+end
