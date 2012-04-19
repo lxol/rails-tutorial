308764900f3aee299018a39c2515d39be805dc51
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-19 17:20:58 +0100

    Order microposts.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
   validates :user_id, presence: true
+  default_scope order: 'microposts.created_at DESC'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -109,4 +109,17 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+  end
 end
