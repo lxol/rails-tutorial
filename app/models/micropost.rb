78d17e1d18095ee63fa082ffdc190ee1e42a6e52
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-19 15:54:25 +0100

    Add user <-> microposts relationships.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,5 @@
 class Micropost < ActiveRecord::Base
+  attr_accessible :content
+  belongs_to :user
+  validates :user_id, presence: true
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = user.microposts.build(content: "Lorem ipsum")
+  end
+  subject { @micropost }
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+
+  its(:user) { should == user }
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
 end
