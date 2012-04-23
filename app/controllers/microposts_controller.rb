8b1084d67062879c998ee11384bda5559df011ad
Alex Olkhovskiy <alex+github@olkhovskiy.com>
2012-04-23 17:47:03 +0100

    Add "Create micropost" machinery. #1

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,6 +5,13 @@ def index
 end
 
 def create
+  @micropost = current_user.microposts.build(params[:micropost])
+  if @micropost.save
+    flash[:success] = "Micropost created!"
+    redirect_to root_path
+  else
+    render 'static_pages/home'
+  end
 end
 
 def destroy
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe "MicropostPages" do
-  describe "GET /micropost_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get micropost_pages_index_path
-      response.status.should be(200)
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+  before { sign_in user }
+
+  describe "micropost creation" do
+    before { visit root_path }
+
+    describe "with invalid information" do
+
+      it "should not create a micropost" do
+        expect { click_button "Post" }.should_not change(Micropost, :count)
+      end
+
+      describe "error messages" do
+        before  { click_button "Post" }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with valid information" do
+      before { fill_in 'micropost_content', with: "Lorem ipsum" }
+      it "should create a micropost" do
+        expect { click_button "Post" }.should change(Micropost, :count).by(1)
+      end
     end
   end
 end
